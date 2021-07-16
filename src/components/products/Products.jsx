079a208509bb62c9f632f3de7d57044e6f4622c2
annpermi <perminova.anna85@gmail.com>
2021-07-16 14:17:12 -0400

    Added images

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import {Grid} from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import Product from './product/Product'
 
 const products = [
-    {id: 1, name: 'Shoes', description: 'Running shoes', price: '$5'},
-    {id: 2, name: 'Macbook', description: 'Apple macbook', price: '$10'}
+    {id: 1, name: 'Shoes', description: 'Running shoes', price: '$5', image: 'https://nb.scene7.com/is/image/NB/ml574es2_nb_02_i?$pdpflexf2$&wid=440&hei=440'},
+    {id: 2, name: 'Macbook', description: 'Apple macbook', price: '$10', image: 'https://as-images.apple.com/is/rfb-macbook-air-silver-select-201902?wid=1000&hei=1000&fmt=jpeg&qlt=95&.v=1548454536059'}
 ]
 const Products = () => {
-    <main>
-        <Grid container justify='center' spacing={4}>
-            {products.map((product) => {
-                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                    <Product product={product}/>
-                </Grid>
-            })}
-        </Grid>
-    </main>
+    return (
+        <main>
+            <Grid container justify='center' spacing={4}>
+                {products.map((product) => (
+                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                        <Product product={product}/>
+                    </Grid>
+                ))}
+            </Grid>
+        </main>
+    )
 }
 
 export default Products;
--- src/components/products/product/Product.jsx
+++ src/components/products/product/Product.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton} from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
-
+import useStyles from './styles';
 
 
 function Product({product}) {
+    //we can use our styles from styles.js
+    const classes = useStyles();
+
     return (
         <Card className={classes.root}>
-            <CardMedia className={classes.media} image='' title={product.title}/>
+            <CardMedia className={classes.media} image={product.image} title={product.title}/>
             <CardContent>
                 <div className={classes.CardContent}>
                     <Typography variant='h5' gutterBottom>
@@ -17,10 +20,15 @@ function Product({product}) {
                         {product.price}
                     </Typography>
                 </div>
-                <Typography variant='h2' color='textSecondary'>
+                <Typography variant='body2' color='textSecondary'>
                         {product.description}
                 </Typography>
             </CardContent>
+            <CardActions disableSpacing className={classes.cardActions}>
+                <IconButton aria-label='Add to Cart'>
+                    <AddShoppingCart /> 
+                </IconButton>
+            </CardActions>
         </Card>
     )
 } 
